db64e8ffe79a26c3617fc5e0b259a7b832f0e399
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-14 13:21:02 -0500

    before and after

--- src/com/theironyard/BankAccountTest.java
+++ src/com/theironyard/BankAccountTest.java
@@ -4,9 +4,21 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount account;
+
+    @org.junit.BeforeClass
+        public static void beforeClass() {
+        System.out.println("This executes before any test cases");
+    }
+
+    @org.junit.Before
+    public void setup() {
+        account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
+
     @org.junit.Test
     public void deposit() throws Exception {
-       BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
        double balance = account.deposit(200.00, true);
        assertEquals(1200.00, balance, 0);
     }
@@ -18,23 +30,31 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance() throws Exception {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_deposit() throws Exception {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() throws Exception {
-        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
         double balance = account.withdraw(200.00, true);
         assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        assertTrue("The account is NOT a checking account", account.isChecking());
+    }
+
+    @org.junit.AfterClass
+    public static void afterClass() {
+        System.out.println("This executes after any test cases");
     }
 
 }
