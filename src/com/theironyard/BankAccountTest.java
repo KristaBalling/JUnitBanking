03eadea25727dcbf7381e8f97fd490f1f7d92687
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-14 12:29:01 -0500

    only one test will fail, the rest have no output

--- src/com/theironyard/BankAccountTest.java
+++ src/com/theironyard/BankAccountTest.java
@@ -6,7 +6,9 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() throws Exception {
-        fail("This test has yet to be implemented");
+       BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+       double balance = account.deposit(200.00, true);
+       assertEquals(1200.00, balance, 0);
     }
 
     @org.junit.Test
@@ -16,12 +18,23 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance() throws Exception {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
-    public void dummyTest() {
-        assertEquals(20, 21);
+    public void getBalance_deposit() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void getBalance_withdraw() throws Exception {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00);
+        double balance = account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
     }
 
 }
